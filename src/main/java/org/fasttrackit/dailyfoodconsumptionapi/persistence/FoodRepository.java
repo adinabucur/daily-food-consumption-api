9603f1e4a76fa95e0756731eca946f82dcd70de3
Adina Bucur <adinaa.bucur@gmail.com>
2019-06-09 11:51:08 +0300

    Get food by id (first endpoint)

--- src/main/java/org/fasttrackit/dailyfoodconsumptionapi/persistence/FoodRepository.java
+++ src/main/java/org/fasttrackit/dailyfoodconsumptionapi/persistence/FoodRepository.java
@@ -3,9 +3,10 @@ package org.fasttrackit.dailyfoodconsumptionapi.persistence;
 import org.fasttrackit.dailyfoodconsumptionapi.domain.Food;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FoodRepository extends PagingAndSortingRepository<Food, Long>{
+public interface FoodRepository extends PagingAndSortingRepository<Food, Long> {
 
     Page<Food> findByNameContainingAndQuantityGreaterThanEqual(
             String partialName, int minimumQuantity, Pageable pageable);
@@ -21,8 +22,8 @@ public interface FoodRepository extends PagingAndSortingRepository<Food, Long>{
     Page<Food> findByNameContaining(String partialName, Pageable pageable);
 
 
-//same result as method above (except the returned columns)
-//    @Query("SELECT id, name FROM food WHERE name LIKE '%?1")
-//    Page<Food> findByPartialName (String partialName, Pageable pageable);
+    //same result as method above (except the returned columns)
+    @Query(value = "SELECT id, name FROM Food food WHERE name LIKE '%?1'")
+    Page<Food> findByPartialName(String partialName, Pageable pageable);
 
 }
---
+++ src/main/java/org/fasttrackit/dailyfoodconsumptionapi/web/FoodController.java
@@ -0,0 +1,30 @@
+package org.fasttrackit.dailyfoodconsumptionapi.web;
+
+import org.fasttrackit.dailyfoodconsumptionapi.domain.Food;
+import org.fasttrackit.dailyfoodconsumptionapi.exception.ResourceNotFoundException;
+import org.fasttrackit.dailyfoodconsumptionapi.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/foods")
+public class FoodController {
+
+    private final FoodService foodService;
+
+    @Autowired
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Food> getFood(@PathVariable("id") long id) throws ResourceNotFoundException {
+        Food response = foodService.getFood(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
